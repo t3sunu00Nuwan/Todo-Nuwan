b0eab01d92bb2ab237fa4c3cf9b772f276fb204d
nuwan98765432 <t3sunu00@students.oamk.fi>
2024-11-01 03:07:47 +0200

    todo part 4 completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
 import './App.css';
 import React from 'react';
-import {useState } from 'react';
+import {useEffect, useState } from 'react';
+import axios from 'axios';
 
+const url = 'http://localhost:3001';
+
+ 
 function App() {
 
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
+//..............................................................
+  useEffect(() => {
+
+    axios.get(url)
+    .then(response => {
+      setTasks(response.data)
+    }).catch(error => {
+      alert(error.response.data.error ? error.response.data.error : error);
+    })
+  } , []);
+
+
+
+
+
+
+
+//..............................................................
   const addTask = () => {
-    setTasks([...tasks, task]);
-    setTask('');
+    axios.post(url + '/create', 
+      {description: task})
+
+    .then(response => {
+      setTasks([...tasks, {id: response.data.id, description: task}]);
+      setTask('');
+    }).catch(error => {
+      alert(error.response.data.error ? error.response.data.error : error);
+    })
   }
 
-  const deleteTask = (deleted) => {
-    const WithoutRemoved = tasks.filter ((item) => item !== deleted);
-    setTasks(WithoutRemoved);
+
+
+  const deleteTask = (id) => {
+    axios.delete(url + '/delete/' + id)
+    .then(response => {
+      const WithoutRemoved = tasks.filter ((item) => item.id !== id);
+      setTasks(WithoutRemoved);
+
+    }).catch(error => {
+      alert(error.response.data.error ? error.response.data.error : error);
+    })
+    
   }
 
 
@@ -38,7 +76,9 @@ function App() {
       <ul>
         {
           tasks.map (item => (
-            <li>{item} <button className = 'delete-button' onClick ={() => deleteTask(item) }> Delete</button></li>
+            <li key={item.id}>{item.description} 
+            <button className = 'delete-button' onClick ={() => deleteTask(item.id) }> Delete</button>
+            </li>
           ))
         }
 
